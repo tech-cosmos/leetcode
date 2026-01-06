60cdc9e349d9681441c014e2c373723308155cc9
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-06 04:56:50 -0500

    Time: 4 ms (69.78%), Space: 23.1 MB (19.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,9 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_map={}
-        res=[]
+        from collections import defaultdict
+        num_map=defaultdict(int)
         for num in nums:
-            if num in num_map:
-                num_map[num]+=1
-            else:
-                num_map[num]=1
-        
+            num_map[num]+=1
         freq=dict(sorted(num_map.items(), key=lambda x: x[1], reverse=True))
         return list(freq.keys())[:k]
         
