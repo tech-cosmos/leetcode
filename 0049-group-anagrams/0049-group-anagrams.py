70737bae83bd468410d22f5f07dbf09ee6473711
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-06 04:23:36 -0500

    Time: 10 ms (86.39%), Space: 22.7 MB (18.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,11 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        char_map={}
         final={}
         for str in strs:
-            chars=[a for a in str]
-            char_map[str]=chars
-            chars=tuple(sorted(chars))
+            chars=tuple(sorted(str))
             if chars in final:
                 final[chars].append(str)
             else:
