86af0bed3f15f410f52af104398213a478715335
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-07 18:42:15 -0500

    Time: 10 ms (40.82%), Space: 19.6 MB (18.75%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        converted=''
-        for char in s.lower():
-            if ord(char) in range(97,123) or ord(char) in range(47,58):
-                converted+=char
-        n=len(converted)
-        l,r=0,n-1
-        for i in range(n//2):
-            if converted[l+i]!=converted[r-i]:
+        l,r=0,len(s)-1
+        while l<r:
+            while not s[l].isalnum() and l<r:
+                l+=1
+            while not s[r].isalnum() and l<r:
+                r-=1
+            if s[l].lower()!=s[r].lower():
                 return False
+            l+=1
+            r-=1
         return True
         
