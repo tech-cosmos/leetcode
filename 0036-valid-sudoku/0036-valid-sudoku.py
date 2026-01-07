2e5d7bf677c66ab6a1840549c5647c2d6d9c1c81
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-06 22:46:58 -0500

    Time: 11 ms (8.91%), Space: 19.6 MB (16.62%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,53 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        from collections import defaultdict
+        valid=True
+        def row_check():
+            for row in board:
+                if not check_valid(row):
+                    print('invalid row', row)
+                    return False
+            return True
+        
+        def col_check():
+            for i in range(9):
+                nums=[row[i] for row in board]
+                if not check_valid(nums):
+                    print('invalid column', nums)
+                    return False
+            return True
+
+        def grid_check():
+            bb=[]
+            for row in board:
+                for j in range(len(row)):
+                    bb.append(row[j])
+            for i in range(9):
+                gc=i//3
+                rem=i%3
+                base=rem*3
+                if gc!=0:
+                    base+=(27*gc)
+                grid=bb[base:base+3]+bb[base+9:base+12]+bb[base+18:base+21]
+                if i==2:
+                    print('Third Grid : ', grid, base, gc)
+                if not check_valid(grid):
+                    print('grid no ', i)
+                    print('invalid grid', grid, gc, base)
+                    return False
+            return True
+        
+        def check_valid(arr):
+            num_map=defaultdict(int)
+            for num in arr:
+                num_map[num]+=1
+            num_map['.']=0
+            for x in list(num_map.values()):
+                if x>1:
+                    return False
+            return True
+
+        if row_check() and col_check() and grid_check():
+            return True
+        else:
+            return False
