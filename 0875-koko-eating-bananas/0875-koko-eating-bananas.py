d9409ec66f17dc30e532631cb7cc2ab2504ac490
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-08 22:22:40 -0500

    Time: 234 ms (6.92%), Space: 20.7 MB (5.48%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l,r=1,max(piles)
+
+        def canEat(speed):
+            time=0
+            for pile in piles:
+                time+=pile//speed
+                if pile%speed:
+                    time+=1
+                if time>h:
+                    return False
+            return True
+        
+        while l<r:
+            mid=(l+r)//2
+            print("Checking for {} at speed {}".format(mid,mid))
+            if canEat(mid):
+                r = mid
+            else:
+                l=mid+1
+        return l
+
+
+        
