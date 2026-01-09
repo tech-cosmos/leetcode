ce15861ab6e537a6704850610df4ca3c79d85d9e
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-09 00:19:24 -0500

    Time: 6 ms (19.26%), Space: 19.7 MB (5.21%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,19 @@
+from bisect import bisect_right
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        final=list(nums1)
+        for n in nums2:
+            i=bisect_right(final,n)
+            final.insert(i,n)
+        m=len(nums1)
+        n=len(nums2)
+        mid=(n+m)//2
+        if (n+m)%2==0:
+            print("Mid 1 : ", final[mid-1])
+            print("Mid 2 : ", final[mid])
+            return (final[mid-1]+final[mid])/2
+        else:
+            return final[mid]
+
+
+        
