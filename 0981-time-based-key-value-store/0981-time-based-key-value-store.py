0dc60c582123929975de62bf96296490b2888f1c
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-09 00:00:19 -0500

    Time: 66 ms (89.77%), Space: 69.2 MB (98.58%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from bisect import bisect_right
 class TimeMap:
 
     def __init__(self):
@@ -13,18 +14,21 @@ class TimeMap:
         if key not in self.store:
             return ""
         arr=self.store[key]
-        l,r=0,len(arr)-1
-        current=""
-        while l<=r:
-            mid=(l+r)//2
-            times_mid, times_value=arr[mid]
-            if timestamp<times_mid:
-                r=mid-1
-            else:
-                print("Setting Current ",times_value)
-                l=mid+1
-                current=times_value
-        return current
+        # l,r=0,len(arr)-1
+        # current=""
+        # while l<=r:
+        #     mid=(l+r)//2
+        #     times_mid, times_value=arr[mid]
+        #     if timestamp<times_mid:
+        #         r=mid-1
+        #     else:
+        #         print("Setting Current ",times_value)
+        #         l=mid+1
+        #         current=times_value
+        i=bisect_right(arr,(timestamp,chr(255)))-1
+        if i>=0:
+            return arr[i][1]
+        return ""
 
 
 
