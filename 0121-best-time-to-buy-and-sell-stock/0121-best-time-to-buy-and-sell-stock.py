23a334353eac022198fc2167ae9d08573e3e7f4e
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-09 20:37:04 -0500

    Time: 36 ms (82.67%), Space: 28.7 MB (5.94%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit=0
+        profit, bp =0, prices[0]
         bp=prices[0]
-        for price in prices:
+        for i,price in enumerate(prices):
+            if i==0:
+                continue
             if price>bp:
                 if profit<price-bp:
                     profit=price-bp
-                    print("Reading profit",profit)
             else:
                 bp=price
-                print("Updating Base Price", bp)
         return profit
