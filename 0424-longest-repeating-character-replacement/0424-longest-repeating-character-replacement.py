a1d0b659a09a995ae67965be5a0e8971b7655c64
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-09 21:28:55 -0500

    Time: 79 ms (96.24%), Space: 19.8 MB (5.64%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -7,7 +7,7 @@ class Solution:
             idx=ord(ch)-ord('A')
             chars[idx]+=1
             maxFreq=max(maxFreq, chars[idx])
-            while (right-left+1)-maxFreq>k:
+            if (right-left+1)-maxFreq>k:
                 chars[ord(s[left])-ord('A')]-=1
                 left+=1
             count=max(count,right-left+1)
