e1c67c72331e1aa1a1d365013dee493dabf4791c
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-11 00:17:17 -0500

    Time: 1 ms (80.58%), Space: 19.4 MB (6.23%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -9,7 +9,9 @@ class Solution:
         curr=dummy
         carry=0
         while l1 or l2 or carry:
-            sum=(l1.val if l1 else 0)+(l2.val if l2 else 0)+carry
+            x=l1.val if l1 else 0
+            y=l2.val if l2 else 0
+            sum=x+y+carry
             curr.next=ListNode(sum%10)
             carry=sum//10
             curr=curr.next
