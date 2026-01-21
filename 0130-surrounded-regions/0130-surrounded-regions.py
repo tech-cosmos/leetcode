f2bdd87129a3bf3643c767334d9704c66497e558
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-21 18:11:39 -0500

    Time: 7 ms (56.54%), Space: 22.3 MB (34.61%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -5,21 +5,20 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         m,n=len(board), len(board[0])
-        safe=[[False]*n for _ in range(m)]
 
         def bfs(starts):
             q=deque()
             for r,c in starts:
                 if board[r][c]=="O":
-                    safe[r][c]=True
+                    board[r][c]="S"
                     q.append((r,c))
             
             while q:
                 r,c=q.popleft()
                 for dr,dc in ((1,0),(-1,0),(0,1),(0,-1)):
                     nr,nc=r+dr, c+dc
-                    if 0<=nr<m and 0<=nc<n and board[nr][nc]=="O" and not safe[nr][nc]:
-                        safe[nr][nc]=True
+                    if 0<=nr<m and 0<=nc<n and board[nr][nc]=="O":
+                        board[nr][nc]="S"
                         q.append((nr,nc))
         
         starts=[(0,c) for c in range(n)]+[(r,0) for r in range(m)]+[(m-1,c) for c in range(n)]+[(r,n-1) for r in range(m)]
@@ -27,5 +26,7 @@ class Solution:
         
         for i in range(m):
             for j in range(n):
-                if board[i][j]=="O" and not safe[i][j]:
+                if board[i][j]=="O":
                     board[i][j]="X"
+                elif board[i][j]=="S":
+                    board[i][j]="O"
