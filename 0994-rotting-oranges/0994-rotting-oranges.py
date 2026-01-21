91ca5e4131de65b9a5104bef79c9f12434f53ff2
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-21 16:07:03 -0500

    Time: 3 ms (79.98%), Space: 19.4 MB (11.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -14,9 +14,9 @@ class Solution:
         while q:
             r,c, time=q.popleft()
 
-            for dr,dc in [(1,0), (-1,0), (0,1), (0,-1)]:
+            for dr,dc in ((1,0), (-1,0), (0,1), (0,-1)):
                 nr,nc=r+dr, c+dc
-                if 0<=nr<m and 0<=nc<n and grid[nr][nc] not in [0,2]:
+                if 0<=nr<m and 0<=nc<n and grid[nr][nc] not in (0,2):
                     grid[nr][nc]=2
                     count=max(count, time+1)
                     q.append((nr,nc,time+1))
