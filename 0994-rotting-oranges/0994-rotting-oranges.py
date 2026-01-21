cca2efbb1b8ade54371494f42f0fa66586b11ec6
Shivam Raj <98623154+tech-cosmos@users.noreply.github.com>
2026-01-21 16:06:11 -0500

    Time: 3 ms (79.98%), Space: 19.4 MB (11.57%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m,n=len(grid), len(grid[0])
+        count=0
+        q=deque()
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==2:
+                    q.append((i,j,0))
+
+        while q:
+            r,c, time=q.popleft()
+
+            for dr,dc in [(1,0), (-1,0), (0,1), (0,-1)]:
+                nr,nc=r+dr, c+dc
+                if 0<=nr<m and 0<=nc<n and grid[nr][nc] not in [0,2]:
+                    grid[nr][nc]=2
+                    count=max(count, time+1)
+                    q.append((nr,nc,time+1))
+        
+        print(grid)
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    return -1
+        
+        return count
